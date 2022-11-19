1d986bdf6bb7b73858224d5becf43cdf540acb93
bluepnwage <a.carty2555@gmail.com>
2022-11-18 20:30:41 -0400

    add viewport meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,9 @@ export default function RootLayout({ children }: ComponentProps) {
     <html lang="en" className={`${kumbhSans.className}`}>
       <head>
         <title>SXM Photography | Yalography</title>
+        <meta charSet="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className="bg-zinc-900 text-gray-300">
         <Header />
