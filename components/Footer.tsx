41996e2299c985bdb766a3125238bb16d8ba8183
bluepnwage <a.carty2555@gmail.com>
2022-11-18 23:37:06 -0400

    responsive footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,25 +3,29 @@ import { Grid } from "./shared";
 export function Footer() {
   return (
     <footer className="border-t flex flex-col items-center border-gray-600 py-10">
-      <Grid width="w-11/12">
-        <div className="col-span-3 space-y-2">
+      <Grid
+        width="w-11/12"
+        gap="gap-10 lg:gap-0"
+        className="place-items-center lg-place-items-start text-center lg:text-start"
+      >
+        <div className="col-span-full lg:col-span-3 space-y-2">
           <p className="font-semibold text-lg">Yalography</p>
           <small className="text-gray-400">© 2022, All rights reserved</small>
         </div>
-        <div className="col-span-3">
+        <div className="col-span-full lg:col-span-3">
           <ul className="space-y-4">
             <li>About </li>
             <li>Projects</li>
             <li>Gallery</li>
           </ul>
         </div>
-        <div className="col-span-3">
+        <div className="col-span-full lg:col-span-3">
           <ul className="space-y-4">
             <li>Contact </li>
             <li>FAQ</li>
           </ul>
         </div>
-        <div className="col-span-3 ">
+        <div className="col-span-full lg:col-span-3">
           <p className="font-semibold text-lg mb-4">Follow us</p>
           <ul className="flex gap-4">
             <li className="rounded-full flex justify-center items-center bg-zinc-800 h-12 w-12">
@@ -53,17 +57,6 @@ export function Footer() {
   );
 }
 
-function Twitter() {
-  return (
-    <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-      <g fill="none" stroke="#ffffff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2">
-        <path d="M0 0h24v24H0z" fill="none" stroke="none" />
-        <path d="M22 4.01c-1 .49-1.98.689-3 .99-1.121-1.265-2.783-1.335-4.38-.737S11.977 6.323 12 8v1c-3.245.083-6.135-1.395-8-4 0 0-4.182 7.433 4 11-1.872 1.247-3.739 2.088-6 2 3.308 1.803 6.913 2.423 10.034 1.517 3.58-1.04 6.522-3.723 7.651-7.742a13.84 13.84 0 0 0 .497-3.753C20.18 7.773 21.692 5.25 22 4.009z" />
-      </g>
-    </svg>
-  );
-}
-
 function Facebook() {
   return (
     <svg height={24} width={24} viewBox="0 0 320 512" xmlns="http://www.w3.org/2000/svg">
@@ -73,8 +66,6 @@ function Facebook() {
     </svg>
   );
 }
-
-import React from "react";
 
 function Instagram() {
   return (
