22298dcce462d5ef0ad52fa607db15a8f159fc73
bluepnwage <a.carty2555@gmail.com>
2022-11-19 21:38:33 -0400

    add svg bg pattern

--- components/home/hero/Hero.tsx
+++ components/home/hero/Hero.tsx
@@ -5,8 +5,8 @@ import styles from "./Hero.module.css";
 
 export function Hero() {
   return (
-    <Section className={styles.hero}>
-      <div className="grid grid-cols-2 gap-5 bg-zinc-800 h-full items-stretch">
+    <Section className={`${styles.hero} border-b border-zinc-700`}>
+      <div className="grid grid-cols-2 gap-5 h-full items-stretch">
         <div className="flex flex-col justify-center p-5 lg:pl-5 col-span-full lg:col-span-1 ">
           <header className="mb-7">
             <h1 className="font-bold text-6xl text-gray-100">
@@ -16,11 +16,10 @@ export function Hero() {
               </span>{" "}
             </h1>
           </header>
-          <p className="leading-loose mb-4 text-lg">
+          <p className="leading-loose mb-4 text-xl">
             Magna in sit ea aliquip nostrud ex nostrud in dolore Lorem aliquip ut. Anim quis consequat nisi
             reprehenderit consequat. Ipsum cillum consequat consectetur qui adipisicing. Incididunt do commodo
-            incididunt excepteur tempor enim non incididunt magna cupidatat. Irure pariatur cupidatat irure esse
-            consectetur aute exercitation. Eiusmod velit ad dolore sint incididunt ipsum enim.
+            incididunt excepteur tempor enim non incididunt magna cupidatat.
           </p>
           <Button>Request Session</Button>
         </div>
--- components/shared/Section/Section.tsx
+++ components/shared/Section/Section.tsx
@@ -1,10 +1,12 @@
 import type { HTMLAttributes } from "react";
 
-interface PropTypes extends HTMLAttributes<HTMLElement> {}
+interface PropTypes extends HTMLAttributes<HTMLElement> {
+  margin?: string;
+}
 
-export function Section({ children, className, ...props }: PropTypes) {
+export function Section({ children, className, margin, ...props }: PropTypes) {
   return (
-    <section {...props} className={`flex flex-col items-center mb-48 ${className}`}>
+    <section {...props} className={`flex flex-col items-center ${margin || "mb-48"} ${className}`}>
       {children}
     </section>
   );
