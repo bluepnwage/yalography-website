e732e4089600bc187b528b630766eee92eb79278
bluepnwage <a.carty2555@gmail.com>
2022-11-21 14:10:27 -0400

    small fixes

--- components/projects/intro/Intro.tsx
+++ components/projects/intro/Intro.tsx
@@ -2,7 +2,7 @@ import { Section, Title } from "@components/shared";
 
 export function Intro() {
   return (
-    <Section margin="mb-0" className={`svg-background text-center py-10 border-b border-zinc-700`}>
+    <Section margin={false} className={`svg-background text-center py-10 border-b border-zinc-700`}>
       <Title className="text-4xl lg:text-6xl text-center">
         See how we deliver the <br />
         <span className="bg-gradient-to-tr from-rose-500 to-red-600 bg-clip-text text-transparent">
