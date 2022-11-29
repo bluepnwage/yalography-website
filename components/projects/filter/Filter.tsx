6302a43003f9a0f31c4cc3887ca30cef9892f772
bluepnwage <a.carty2555@gmail.com>
2022-11-28 20:06:56 -0400

    add theme switch support for projects page

--- components/projects/filter/Filter.tsx
+++ components/projects/filter/Filter.tsx
@@ -3,7 +3,7 @@ import styles from "./Filter.module.css";
 
 export function Filter() {
   return (
-    <div className="border-b py-5 mb-16 px-16 justify-between gap-5 items-center lg:items-start lg:gap-0 flex-col lg:flex-row flex border-zinc-700">
+    <div className="border-b h-16 mb-16 px-16 justify-between gap-5 items-center md:items-items-center md:gap-0 flex-col md:flex-row flex border-zinc-200 dark:border-zinc-700">
       <div className="flex gap-5 items-center">
         <div className="flex gap-2">
           <Icon.FilterIcon />
@@ -12,12 +12,12 @@ export function Filter() {
         <hr className="border border-zinc-700 h-5" />
         <button>Clear all</button>
       </div>
-      <p className="space-y-2 lg:space-y-0 lg:space-x-4 relative">
+      <p className="flex gap-2 items-center relative">
         <label htmlFor="search">Search</label>
         <input
           type={"search"}
           id="search"
-          className={`${styles.input} relative appearance-none px-4 py-1 outline-none rounded-sm bg-zinc-700 border border-gray-700 focus:border-red-500 duration-200 ease-out`}
+          className={`${styles.input} relative appearance-none px-4 py-1 outline-none rounded-sm bg-zinc-100 border-gray-200 dark:bg-zinc-700 border dark:border-gray-700 focus:border-red-500 duration-200 ease-out`}
         />
       </p>
     </div>
--- components/projects/projectList/Project.tsx
+++ components/projects/projectList/Project.tsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 import pixel from "@public/pixel.jpg";
-import { Badge, Anchor } from "@components/shared";
+import { Badge, Anchor, Card, Title } from "@components/shared";
 
 export function Project() {
   return (
-    <div className="bg-zinc-800 rounded-md flex flex-col col-span-full lg:col-span-3 gap-4 overflow-hidden ">
-      <figure className="basis-1/3">
+    <Card className="flex flex-col col-span-full lg:col-span-3 gap-4 overflow-hidden ">
+      <figure className="basis-1/3 -m-4">
         <Image src={pixel} alt={""} className="h-full w-full" />
       </figure>
       <div className="p-4 space-y-4">
         <div className="flex justify-between basis-2/3">
-          <h3 className="font-bold text-2xl text-gray-100 ">Pixel art</h3>
+          <Title order={'h3'} className="text-2xl">Pixel art</Title>
           <Badge color={"violet"}>Wedding</Badge>
         </div>
         <p>
@@ -21,6 +21,6 @@ export function Project() {
           View project
         </Anchor>
       </div>
-    </div>
+    </Card>
   );
 }
