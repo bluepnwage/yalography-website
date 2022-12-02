6914b3c2ee10e611aee855d1a577239e3cc8747a
bluepnwage <a.carty2555@gmail.com>
2022-12-02 14:22:45 -0400

    small fixes

--- components/about/logo-cloud/LogoCloud.tsx
+++ components/about/logo-cloud/LogoCloud.tsx
@@ -13,7 +13,7 @@ export function LogoCloud() {
     <Section margin={false} className="bg-gray-50 dark:bg-zinc-800 px-5 py-16">
       <div className="w-full gap-5 flex ">
         <div className="basis-3/5 grow pr-5 space-y-4">
-          <Title order={"h2"}>The world's most innovative companies use Yalography</Title>
+          <Title order={"h2"}>The world&apos;s most innovative companies use Yalography</Title>
           <p className="text-xl">
             Sunt eu sunt duis commodo ad irure. In enim enim labore occaecat irure excepteur enim cupidatat sit commodo
             qui ipsum elit. Aliqua sunt aute excepteur fugiat dolor commodo.
