09d8f9d73ad378fbf5777c21faa647896d7d3b7c
bluepnwage <a.carty2555@gmail.com>
2022-12-01 23:08:02 -0400

    add page intro to bookings page

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,11 +1,18 @@
-import { ContactForm,  } from "@components/bookings";
+import { ContactForm } from "@components/bookings";
 import { PriceCalculator } from "@components/bookings/client";
 import { BookingsFormContainer } from "@components/bookings/form/Form";
 import { Section, Title } from "@components/shared";
+import { PageIntro } from "@components/PageIntro";
 
 export default function BookingsPage() {
   return (
     <>
+      <PageIntro>
+        Reserve your{" "}
+        <span className="bg-gradient-to-tr from-rose-500 to-red-600 bg-clip-text text-transparent">
+          photoshoot today
+        </span>
+      </PageIntro>
       <Section className="mt-10">
         <Title order={"h2"}>Price calculator</Title>
         <PriceCalculator />
