890d8fa3233a013fa8e632b0212cdf8a96d11f28
bluepnwage <a.carty2555@gmail.com>
2022-12-10 11:33:08 -0400

    add size prop to badge

--- components/projects/projectList/Project.tsx
+++ components/projects/projectList/Project.tsx
@@ -9,11 +9,13 @@ export function Project() {
         <Image src={pixel} alt={""} className="h-full w-full" />
       </figure>
       <div className="p-2 space-y-4">
-        <div className="flex flex-wrap grow justify-between basis-2/3">
+        <div className="flex flex-wrap grow justify-between basis-2/3 items-center">
           <Title order={"h3"} className="text-2xl">
             Pixel art
           </Title>
-          <Badge color={"violet"}>Wedding</Badge>
+          <Badge size={"sm"} color={"violet"}>
+            Wedding
+          </Badge>
         </div>
         <p>
           Quis nostrud velit aliquip sint aliquip consectetur. Consequat aliqua irure elit do culpa non commodo id non
--- components/shared/Badge/Badge.tsx
+++ components/shared/Badge/Badge.tsx
@@ -2,7 +2,7 @@ import type { ComponentPropsWithoutRef } from "react";
 import { cva } from "cva";
 import type { VariantProps } from "cva";
 
-const styles = cva("flex justify-center font-semibold rounded-full items-center px-3 py-1", {
+const styles = cva("flex h-fit justify-center font-semibold rounded-full items-center ", {
   variants: {
     color: {
       red: "bg-red-200 text-red-600 dark:bg-red-500 dark:text-red-200 dark:bg-opacity-50",
@@ -11,18 +11,25 @@ const styles = cva("flex justify-center font-semibold rounded-full items-center 
       orange: "bg-orange-200 text-orange-600 dark:bg-orange-500 dark:text-orange-200 dark:bg-opacity-50",
       green: "bg-green-200 text-green-600 dark:bg-green-500 dark:text-green-200 dark:bg-opacity-50",
       violet: "bg-violet-200 text-violet-600 dark:bg-violet-500 dark:text-violet-200 dark:bg-opacity-50"
+    },
+    size: {
+      sm: "text-sm px-3 py-0",
+      md: "text-md px-4 py-1",
+      lg: "text-lg px-6 py-2",
+      xl: "text-xl px-8 py-4"
     }
   },
   defaultVariants: {
-    color: "red"
+    color: "red",
+    size: "md"
   }
 });
 
 type BadgeProps = Omit<ComponentPropsWithoutRef<"div">, "color"> & VariantProps<typeof styles>;
 
-export function Badge({ children, className, color }: BadgeProps) {
+export function Badge({ children, className, color, size }: BadgeProps) {
   return (
-    <div className={styles({ className, color })}>
+    <div className={styles({ className, color, size })}>
       <span>{children}</span>
     </div>
   );
