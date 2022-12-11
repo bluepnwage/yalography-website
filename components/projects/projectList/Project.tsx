c04e99b752ca30907a3023597b5bf9254313617f
bluepnwage <a.carty2555@gmail.com>
2022-12-11 11:26:00 -0400

    fix full width link

--- components/projects/projectList/Project.tsx
+++ components/projects/projectList/Project.tsx
@@ -5,9 +5,11 @@ import { Badge, Anchor, Card, Title } from "@components/shared";
 export function Project() {
   return (
     <Card className="flex flex-col col-span-full lg:col-span-3 gap-4 overflow-hidden ">
-      <figure className="basis-1/3 -m-4">
-        <Image src={pixel} alt={""} className="h-full w-full" />
-      </figure>
+      <Anchor href={"/projects/random-project"} className="basis-1/3 -m-4">
+        <figure className="w-full h-full">
+          <Image src={pixel} alt={""} className="h-full w-full" />
+        </figure>
+      </Anchor>
       <div className="p-2 space-y-4">
         <div className="flex flex-wrap grow justify-between basis-2/3 items-center">
           <Title order={"h3"} className="text-2xl">
@@ -21,7 +23,7 @@ export function Project() {
           Quis nostrud velit aliquip sint aliquip consectetur. Consequat aliqua irure elit do culpa non commodo id non
           voluptate qui velit reprehenderit. Ea laborum do Lorem ut consequat aute.
         </p>
-        <Anchor href={"/projects/random-project"} className="block">
+        <Anchor href={"/projects/random-project"} className="block w-fit">
           View project
         </Anchor>
       </div>
