c117df97bb21336f6cb2bb0575b4025a3f2c37eb
bluepnwage <a.carty2555@gmail.com>
2022-12-18 21:36:40 -0400

    force page to be dynamic

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,6 +3,7 @@ import { Table } from "@components/admin/home/Table";
 import { Stats } from "@components/admin/home/Stats";
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export default function AdminPage() {
   return (
