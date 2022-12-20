723f3a25a4f793c5b945ecba21e0268fb9924db5
bluepnwage <a.carty2555@gmail.com>
2022-12-19 20:49:30 -0400

    add line breaks

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -5,8 +5,9 @@ export default function GalleryPage() {
   return (
     <>
       <PageIntro>
-        Captivating Photos: <br /> A
-        <span className="bg-gradient-to-tr from-rose-500 to-red-600 bg-clip-text text-transparent">Visual Journey</span>
+        Captivating Photos: <br /> A{" "}
+        <span className="bg-gradient-to-tr from-rose-500 to-red-600 bg-clip-text text-transparent">Visual Journey</span>{" "}
+        <br />
         Through Our Gallery
       </PageIntro>
       <Gallery />
