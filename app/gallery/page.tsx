f4d92f56a544ac961ccc88bcb09a1cdcb2239ff0
bluepnwage <a.carty2555@gmail.com>
2022-12-19 20:46:34 -0400

    update gallery heading

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -5,10 +5,9 @@ export default function GalleryPage() {
   return (
     <>
       <PageIntro>
-        Very cool and <br />
-        <span className="bg-gradient-to-tr from-rose-500 to-red-600 bg-clip-text text-transparent">
-          inspiring title
-        </span>
+        Captivating Photos: <br /> A
+        <span className="bg-gradient-to-tr from-rose-500 to-red-600 bg-clip-text text-transparent">Visual Journey</span>
+        Through Our Gallery
       </PageIntro>
       <Gallery />
     </>
