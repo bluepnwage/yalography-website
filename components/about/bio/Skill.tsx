7cfbc214cc55681ed4aed98ef706e35f32c88958
bluepnwage <a.carty2555@gmail.com>
2022-12-25 21:22:17 -0400

    test glow effect

--- components/about/bio/Skill.tsx
+++ components/about/bio/Skill.tsx
@@ -3,9 +3,8 @@ import { ThemeIcon, Card } from "@components/shared";
 export function Skill() {
   return (
     <Card
-      gradientBorder
-      containerStyles="col-span-full lg:col-span-1 lg:last-of-type:col-span-full rounded-md"
-      className="flex items-center gap-2 h-full w-full rounded-md"
+      glow
+      className="col-span-full lg:col-span-1 lg:last-of-type:col-span-full  flex items-center gap-2 h-full w-full rounded-md"
     >
       <ThemeIcon>Hello</ThemeIcon>
       <p>Adobe photoshop</p>
