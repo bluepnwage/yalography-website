43ae0571e1059d8f8beb0fb9c24b61fe8bf3002b
bluepnwage <a.carty2555@gmail.com>
2023-01-03 15:20:52 -0400

    fix tsc error

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,7 +1,15 @@
 import { PageIntro } from "@components/PageIntro";
-import { Gallery } from "@components/gallery";
+import { Section } from "@components/shared";
+import prisma from "@lib/prisma";
 
-export default function GalleryPage() {
+async function getImages() {
+  await prisma.$connect();
+  const images = await prisma.images.findMany();
+  await prisma.$disconnect();
+  return images;
+}
+export default async function GalleryPage() {
+  const images = await getImages();
   return (
     <>
       <PageIntro>
@@ -10,7 +18,20 @@ export default function GalleryPage() {
         <br />
         Through Our Gallery
       </PageIntro>
-      <Gallery />
+      <Section className="mt-20">
+        <div
+          style={{ gridTemplateColumns: "repeat(auto-fit, minmax(240px, 1fr))", gridAutoRows: "240px" }}
+          className="grid grid-flow-dense gap-3 w-11/12"
+        >
+          {images.map((image, key) => {
+            return (
+              <figure key={key}>
+                <img src={image.url} alt={image.alt || ""} />
+              </figure>
+            );
+          })}
+        </div>
+      </Section>
     </>
   );
 }
