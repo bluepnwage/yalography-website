cbd2dc23c492993b1ef731af9a5b78e7ea993e05
bluepnwage <a.carty2555@gmail.com>
2023-01-12 14:49:46 -0400

    small change

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -7,6 +7,8 @@ import prisma from "@lib/prisma";
 //Assets
 import pixel from "@public/pixel.jpg";
 
+export const dynamicParams = false;
+
 export async function generateStaticParams() {
   await prisma.$connect();
   const ids = await prisma.projects.findMany({ where: { published: true }, select: { id: true } });
