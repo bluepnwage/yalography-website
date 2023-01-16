5ea4aa81dc40e7bace87f12445629ebcc95d1b1e
bluepnwage <a.carty2555@gmail.com>
2023-01-16 10:35:32 -0400

    option to reschedule bookings

--- components/admin/bookings/dynamic/Menu.tsx
+++ components/admin/bookings/dynamic/Menu.tsx
@@ -6,12 +6,13 @@ import { DotsVertical, CircleCheck, Trash, CalendarTime } from "@lib/icons";
 import dynamic from "next/dynamic";
 
 const Dialog = dynamic(() => import("@components/shared/Dialog").then((mod) => mod.Dialog));
+const DatePicker = dynamic(() => import("@components/shared/DatePicker/DatePicker").then((mod) => mod.DatePicker));
 
 import { useRouter } from "next/navigation";
 import { useRouteRefresh } from "@lib/hooks/useRouteRefresh";
 import { useToggle } from "@lib/hooks/useToggle";
 
-import type { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 
 type MenuProps = {
   status: "pending" | "approved";
@@ -24,6 +25,8 @@ export function BookingMenu({ status, id }: MenuProps) {
   const [loading, toggle] = useToggle();
   const router = useRouter();
   const [lazyLoad, lazyLoadToggle] = useToggle();
+  const [rescheduleDialog, rescheduleToggle] = useToggle();
+  const [newDate, setNewDate] = useState<Date | null>(null);
 
   //This function is called to create in order in the database after marking a
   //booking as complete
@@ -99,6 +102,36 @@ export function BookingMenu({ status, id }: MenuProps) {
     }
   };
 
+  const onReschedule = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const time = new FormData(e.currentTarget).get("time");
+    toggle.on();
+
+    const { toast } = await import("react-toastify");
+    try {
+      const res = await fetch("/api/bookings", {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ date: newDate, time, id })
+      });
+      const json = await res.json();
+      if (res.ok) {
+        refresh();
+        toast.success("Booking succesfully updated. Don't forget to send your customer an email!");
+        setNewDate(null);
+        rescheduleToggle.off();
+      } else {
+        throw new Error(json.message, { cause: json.error });
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      }
+    } finally {
+      toggle.off();
+    }
+  };
+
   const onComplete = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget).get("quote");
@@ -133,14 +166,25 @@ export function BookingMenu({ status, id }: MenuProps) {
 
   return (
     <>
-      <Dialog title="Revenue" open={dialog} onOpenChange={dialogToggle.set}>
-        <form onSubmit={onComplete}>
-          <Input required step={"any"} label="Amount made" id="quote" name="quote" type="number" />
-          <Button disabled={isLoading} className="mt-4" intent={"accept"}>
-            Submit
-          </Button>
-        </form>
-      </Dialog>
+      {lazyLoad && (
+        <Dialog title="Revenue" open={dialog} onOpenChange={dialogToggle.set}>
+          <form onSubmit={onComplete}>
+            <Input required step={"any"} label="Amount made" id="quote" name="quote" type="number" />
+            <Button disabled={isLoading} className="mt-4" intent={"accept"}>
+              Submit
+            </Button>
+          </form>
+        </Dialog>
+      )}
+      {lazyLoad && (
+        <Dialog open={rescheduleDialog} onOpenChange={rescheduleToggle.set} title="Reschedule booking">
+          <form className="space-y-4" onSubmit={onReschedule}>
+            <DatePicker value={newDate} onChange={setNewDate} required name="reschedule" minDate={new Date()} />
+            <Input required type={"time"} label="Time" id="time" name="time" />
+            <Button disabled={!newDate || isLoading}>Submit</Button>
+          </form>
+        </Dialog>
+      )}
       <Dropdown>
         <Dropdown.Trigger>
           <button aria-label="Manage booking">
@@ -149,7 +193,7 @@ export function BookingMenu({ status, id }: MenuProps) {
         </Dropdown.Trigger>
         <Dropdown.Content>
           {status === "approved" && (
-            <Dropdown.Item onClick={dialogToggle.on}>
+            <Dropdown.Item onMouseEnter={!lazyLoad ? lazyLoadToggle.on : undefined} onClick={dialogToggle.on}>
               {" "}
               <CircleCheck size={16} className="stroke-yellow-500 inline-block mr-2" />
               Mark as complete
@@ -162,7 +206,7 @@ export function BookingMenu({ status, id }: MenuProps) {
                 <CircleCheck size={16} className="stroke-yellow-500 inline-block mr-2" />
                 Approve booking
               </Dropdown.Item>
-              <Dropdown.Item>
+              <Dropdown.Item onClick={rescheduleToggle.on} onMouseEnter={!lazyLoad ? lazyLoadToggle.on : undefined}>
                 {" "}
                 <CalendarTime size={16} className="stroke-yellow-500 inline-block mr-2" />
                 Reschedule booking
