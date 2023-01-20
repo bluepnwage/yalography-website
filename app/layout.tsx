3025bf451cd465b68ce4d53dc2512c49be5acd16
bluepnwage <a.carty2555@gmail.com>
2023-01-19 22:01:24 -0400

    use next-font

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,13 @@ import { Header } from "@components/Navbar";
 import { ScrollToTop } from "@components/ScrollToTop";
 import { Suspense } from "react";
 import { ToastProvider } from "@components/ToastProvider";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "optional",
+  variable: "--font-inter"
+});
 
 type PropTypes = {
   children: React.ReactNode;
@@ -11,13 +18,7 @@ type PropTypes = {
 
 export default function RootLayout({ children }: PropTypes) {
   return (
-    <html lang="en" className="font-sans dark dark-mode">
-      <head>
-        <link
-          href="https://fonts.googleapis.com/css?family=Inter:100,200,300,regular,500,600,700,800,900"
-          rel="stylesheet"
-        />
-      </head>
+    <html lang="en" className={`dark dark-mode ${inter.variable}`}>
       <body className="dark:bg-zinc-900 bg-gray-100 text-gray-900 dark:text-gray-300 duration-200 ease-out">
         <Suspense fallback={null}>
           <ToastProvider />
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      sans: ["Inter"]
+      sans: ["var(--font-inter)"]
     },
     extend: {
       keyframes: {
