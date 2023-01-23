c69e06015d06eee314c91de6b69d8e5622626d77
bluepnwage <a.carty2555@gmail.com>
2023-01-23 17:43:18 -0400

    create loading ui for gallery and project pages in dashboard

---
+++ app/admin/gallery/loading.tsx
@@ -0,0 +1,69 @@
+import { Grid, Skeleton, Card, FlexContainer } from "@components/shared";
+
+export default function GalleryLoading() {
+  return (
+    <>
+      <Grid fullWidth className="mb-10">
+        <Card className="col-span-4 relative overflow-hidden">
+          <Skeleton.Shimmer />
+          <FlexContainer className="mb-2 justify-between items-center">
+            <Skeleton className="h-3 w-1/4" />
+            <Skeleton className="h-5 w-3" />
+          </FlexContainer>
+          <Skeleton className="h-3 w-1/4" />
+        </Card>
+        <Card className="col-span-4 relative overflow-hidden">
+          <Skeleton.Shimmer />
+          <FlexContainer className="mb-2 justify-between items-center">
+            <Skeleton className="h-3 w-1/4" />
+            <Skeleton className="h-5 w-3" />
+          </FlexContainer>
+          <Skeleton className="h-3 w-1/4" />
+        </Card>
+        <Card className="col-span-4 relative overflow-hidden">
+          <Skeleton.Shimmer />
+          <FlexContainer className="mb-2 justify-between items-center">
+            <Skeleton className="h-3 w-1/4" />
+            <Skeleton className="h-5 w-3" />
+          </FlexContainer>
+          <Skeleton className="h-3 w-1/4" />
+        </Card>
+      </Grid>
+      <Grid fullWidth>
+        <Card className="col-span-4 flex flex-col gap-4 h-64 relative overflow-hidden">
+          <Skeleton.Shimmer />
+          <Skeleton className="basis-3/4 -mx-4 -mt-4" />
+          <div className="grow space-y-2">
+            <Skeleton className="h-4 w-2/4" />
+            <FlexContainer className="justify-between">
+              <Skeleton className="h-3 w-1/4" />
+              <Skeleton className="h-3 w-1/5" />
+            </FlexContainer>
+          </div>
+        </Card>
+        <Card className="col-span-4 flex flex-col gap-4 h-64 relative overflow-hidden">
+          <Skeleton.Shimmer />
+          <Skeleton className="basis-3/4 -mx-4 -mt-4" />
+          <div className="grow space-y-2">
+            <Skeleton className="h-4 w-2/4" />
+            <FlexContainer className="justify-between">
+              <Skeleton className="h-3 w-1/4" />
+              <Skeleton className="h-3 w-1/5" />
+            </FlexContainer>
+          </div>
+        </Card>
+        <Card className="col-span-4 flex flex-col gap-4 h-64 relative overflow-hidden">
+          <Skeleton.Shimmer />
+          <Skeleton className="basis-3/4 -mx-4 -mt-4" />
+          <div className="grow space-y-2">
+            <Skeleton className="h-4 w-2/4" />
+            <FlexContainer className="justify-between">
+              <Skeleton className="h-3 w-1/4" />
+              <Skeleton className="h-3 w-1/5" />
+            </FlexContainer>
+          </div>
+        </Card>
+      </Grid>
+    </>
+  );
+}
