bf7a116fe180df57c8dc147bdb34bcde444aec2b
bluepnwage <a.carty2555@gmail.com>
2023-01-29 20:12:06 -0400

    small change

--- components/ScrollToTop.tsx
+++ components/ScrollToTop.tsx
@@ -1,18 +1,18 @@
 "use client";
 import { useEffect } from "react";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import { useRef } from "react";
 
 export function ScrollToTop() {
   const path = usePathname();
-  const [isFirstMount, setFirstMount] = useState(true);
+  const isFirstMount = useRef(true);
 
   useEffect(() => {
-    setFirstMount(false);
+    isFirstMount.current = false;
   }, []);
 
   useEffect(() => {
-    if (isFirstMount) return;
+    if (isFirstMount.current) return;
     window.scrollTo({ top: 0, behavior: "auto" });
   }, [path]);
   return <></>;
