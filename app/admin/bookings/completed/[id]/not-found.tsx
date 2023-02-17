9216b911c8c746939c683ce77d4a87e3275a2f2a
bluepnwage <a.carty2555@gmail.com>
2023-02-17 11:26:55 -0400

    create 404 pages for dynamic admin segments

--- app/admin/bookings/completed/[id]/not-found.tsx
+++ app/admin/bookings/completed/[id]/not-found.tsx
@@ -1,4 +1,5 @@
 import { AdminNotFound } from "@components/admin/AdminNotFound";
-export default function NotFound() {
+
+export default function NotFoundPage() {
   return <AdminNotFound type="Booking" />;
 }
---
+++ components/admin/AdminNotFound.tsx
@@ -0,0 +1,17 @@
+type PropTypes = {
+  type: "Task list" | "Project" | "Folder" | "Booking";
+};
+export function AdminNotFound({ type }: PropTypes) {
+  return (
+    <>
+      <section className="grid min-h-full place-items-center  py-24 px-6 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-semibold text-red-600 dark:text-red-500">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-200 sm:text-5xl">
+            {type} not found
+          </h1>
+        </div>
+      </section>
+    </>
+  );
+}
