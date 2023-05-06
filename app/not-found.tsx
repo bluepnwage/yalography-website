d4ee0feb23c4877e46dc3f0a293532ca30b9fe59
bluepnwage <a.carty2555@gmail.com>
2023-05-06 16:06:35 -0400

    migrate to global not found page in app dir

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,20 +1,15 @@
-import "../styles/globals.css";
 import notFound from "@public/404.jpg";
 import Image from "next/image";
-import Head from "next/head";
 import Link from "next/link";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Page not found"
+};
 
 export default function NotFoundPage() {
   return (
     <>
-      <Head>
-        <title>404 - Page Not Found</title>
-        <link rel="icon" href="/logo.png" type="image/png" />
-        <link
-          href="https://fonts.googleapis.com/css?family=Inter:100,200,300,regular,500,600,700,800,900"
-          rel="stylesheet"
-        />
-      </Head>
       <div className="h-screen bg-zinc-900 text-gray-100 ">
         <div className="grid grid-cols-2 h-full">
           <div className="flex justify-center px-10 flex-col">
@@ -22,7 +17,7 @@ export default function NotFoundPage() {
             <h1 className="font-sans text-6xl font-bold mb-4">Page not found</h1>
             <p className="text-gray-400 mb-10">Sorry, we couldn&apos;t find the page you were looking for.</p>
             <Link href={"/"} className="text-yellow-400">
-              Go back to home →
+              Go back to homepage →
             </Link>
           </div>
           <figure className="overflow-hidden object-cover h-full">
