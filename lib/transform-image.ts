7cb46a54a1fa30d5e97f9dcab73b38b1c360b9c7
bluepnwage <a.carty2555@gmail.com>
2023-05-17 16:15:30 -0400

    add folder path to transform function

--- lib/transform-image.ts
+++ lib/transform-image.ts
@@ -1,5 +1,5 @@
 const cloud_name = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
 
 export function transformImage(transform: string, public_id: string, format: string) {
-  return `https://res.cloudinary.com/${cloud_name}/image/upload/c_scale,${transform}/${public_id}.${format}`;
+  return `https://res.cloudinary.com/${cloud_name}/image/upload/c_scale,${transform}/yalo-preview/${public_id}.${format}`;
 }
