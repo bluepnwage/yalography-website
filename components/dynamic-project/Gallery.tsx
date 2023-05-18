cd3929e836401bf770b1931b82294b6077bcd7cf
Agis Carty <91766912+bluepnwage@users.noreply.github.com>
2023-05-18 14:27:41 -0400

    update project page design (#9)
    * update project page design

--- components/dynamic-project/Gallery.tsx
+++ components/dynamic-project/Gallery.tsx
@@ -1,6 +1,8 @@
 import { Title } from "@components/shared";
 import { Image } from "@components/shared/Image";
 import { transformImage } from "@lib/transform-image";
+import { cx } from "cva";
+
 import type { Images } from "@prisma/client";
 import styles from "./styles.module.css";
 type PropTypes = {
@@ -8,16 +10,19 @@ type PropTypes = {
 };
 
 export function Gallery({ images }: PropTypes) {
+  const sortedImages = images.sort(img => {
+    return img.width / img.height > 1 ? 1 : -1;
+  });
   return (
     <>
-      <div className="text-center space-y-2 py-10">
+      <div className="text-center space-y-2 py-10 mt-10 lg:mt-20">
         <Title order={"h2"} color={"red"} size={"md"}>
           Gallery
         </Title>
         <Title order={"h3"}>See the images that brought this project to life</Title>
       </div>
-      <section className={styles.grid}>
-        {images.map(image => {
+      <section className={cx(styles.grid, "mx-auto w-11/12 lg:w-3/5")}>
+        {sortedImages.map(image => {
           const fit = image.width > image.height ? `${styles.colSpan} ${styles.rowSpan}` : styles.rowSpan;
           return (
             <Image
