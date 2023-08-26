c3c1ae7f2df46c58a627cf882625ef3152829237
Agis Carty <a.carty2555@gmail.com>
2023-08-26 10:28:48 -0400

    fix nav styles in light mode

--- components/admin/NavLink.tsx
+++ components/admin/NavLink.tsx
@@ -15,12 +15,15 @@ export function NavLink({ href, label, Icon }: PropTypes) {
     <li
       data-active={path === href}
       className={cx(
-        "w-full rounded-md group hover:text-gray-800 hover:bg-slate-100 dark:hover:text-gray-100 dark:hover:bg-primary-600/20 duration-200 ease-out",
-        "data-[active=true]:font-medium data-[active=true]:bg-primary-600/20 data-[active=true]:text-primary-200"
+        "w-full rounded group  hover:text-gray-800 hover:bg-slate-100 dark:hover:text-gray-100 dark:hover:bg-primary-600/20 duration-200 ease-out",
+        "data-[active=true]:font-medium data-[active=true]:dark:bg-primary-600/20 data-[active=true]:dark:text-primary-200",
+        "data-[active=true]:bg-primary-200/20 data-[active=true]:text-primary-600"
       )}
     >
       <Link href={href} className="px-1 py-2 flex items-center">
-        <span className="inline-block mr-2 group-data-[active=true]:text-primary-200">{Icon}</span>
+        <span className="inline-block mr-2 group-data-[active=true]:text-primary-600 group-data-[active=true]:dark:text-primary-200">
+          {Icon}
+        </span>
         {label}
       </Link>
     </li>
