c716b215f5a0ea2a7842098534597fc63fe76370
Agis Carty <a.carty2555@gmail.com>
2023-11-14 09:00:06 -0400

    fix reschedule template

--- components/reschedule-template.tsx
+++ components/reschedule-template.tsx
@@ -8,11 +8,11 @@ type PropTypes = {
 export function RescheduleTemplate({ firstName, lastName, dates, date, time }: PropTypes) {
   return (
     <p>
-      Dear {firstName} {lastName}, <br /> Your phototoshoot reservation for {date} {time} has to be
-      rescheduled. <br />
-      Unfortunately, we&apos;re either over booked or unavailable on that day. <br /> We understand that this
-      may be disappointing, and we apologize for any inconvenience this may have caused. <br />{" "}
-      <span className="block mb-1">Available dates</span>
+      Dear {firstName} {lastName}, <br /> We apologize for the inconvenience, but we regret to inform you that
+      your photoshoot reservation for {date}, {time} will need to be rescheduled. Unfortunately, we are either
+      over booked or unavailable on that day.
+      <br /> We understand that this may be disappointing, and we apologize for any inconvenience this may
+      have caused. <br /> <span className="block mb-1">Available dates:</span>
       <ul>
         {dates.map(date => {
           return <li key={date}>{date}</li>;
